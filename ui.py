a853df8902d15e4591f72bee460962181bf9e17a
Dariusz Morzuch <dmorzuch@wp.pl>
2024-01-11 05:55:13 +0100

    w pełni połączone z grafem

--- ui.py
+++ ui.py
@@ -4,9 +4,26 @@ from create import route_tour
 
 
 def draw_line(draw, start, end, color="red", width=2):
-    start = change_to_distance(start)
-    end = change_to_distance(end)
     draw.line([start, end], fill=color, width=width)
+
+
+def to_shelf(shelf_num, cord, num):
+    if num == 1:
+        if int(cord[1]) % 2 == 0:
+            av = -1
+        else:
+            av = 1
+        she = (int(shelf_num) % 8 + 1)/2
+        cord = change_to_distance(cord)
+        return ((((cord[0]-60)/11)+she/4*av*11.25)*11+60, cord[1])
+    else:
+        if int(cord[1]) % 2 == 0:
+            av = 1
+        else:
+            av = -1
+        she = (int(shelf_num) % 8 + 1)/2
+        cord = change_to_distance(cord)
+        return ((((cord[0]-60)/11)+(5-she)/4*av*11.25)*11+60, cord[1])
 
 
 def draw_lines():
@@ -15,11 +32,39 @@ def draw_lines():
     draw_li = ImageDraw.Draw(drawn_plan)
 
     for i in range(len(list_of_navigation[line_iterator])-1):
-        draw_line(draw_li, list_of_navigation[line_iterator][i], list_of_navigation[line_iterator][i+1])
-    x,y = change_to_distance(list_of_navigation[line_iterator][0])
-    worker_label.place(x=x/1.68 - 0, y=y/1.65 + 10)
+        if i == 0 and len(list_of_navigation[line_iterator][i]) == 2:
+            draw_line(draw_li, to_shelf(list_of_navigation[line_iterator][i], list_of_navigation[line_iterator][i+1], -1),
+                      change_to_distance(list_of_navigation[line_iterator][i+1]))
+            continue
+        if len(list_of_navigation[line_iterator][i+1]) == 2:
+            draw_line(draw_li, to_shelf(list_of_navigation[line_iterator][i+1], list_of_navigation[line_iterator][i], 1),change_to_distance(list_of_navigation[line_iterator][i]))
+            continue
+        draw_line(draw_li, change_to_distance(list_of_navigation[line_iterator][i]),
+                  change_to_distance(list_of_navigation[line_iterator][i+1]))
+
+    if len(list_of_navigation[line_iterator][0]) == 2:
+        change_to_distance(list_of_navigation[line_iterator][1])
+        cz = to_shelf(list_of_navigation[line_iterator][0], list_of_navigation[line_iterator][1], -1)
+        av = int(list_of_navigation[line_iterator][1][1]) % 2
+        she = ((int(list_of_navigation[line_iterator][0]) % 8)+1)/2
+        x=0
+        y=0
+        if av == 0:
+            av = 1
+        else:
+            av = -1
+    else:
+        x, y = change_to_distance(list_of_navigation[line_iterator][0])
+        av = 0
+        cz = [0, 0]
+    worker_label.place(x=(x+cz[0])/1.68 - 0, y=(y+cz[1])/1.65 + 10)
 
     global warehouse_plan
+    
+    text_var.set("Go to: H" + list_of_navigation[line_iterator][-2][0] + "0" + list_of_navigation[line_iterator][-2][1] + "A" + list_of_navigation[line_iterator][-1])
+
+    # Create a Label widget and associate it with the StringVar
+    text_label.config(textvariable=text_var)
     warehouse_plan = ImageTk.PhotoImage(drawn_plan.resize(
         (int(plan_width * k), int(plan_height * k))))
     warehouse_label.config(image=warehouse_plan)
@@ -71,24 +116,29 @@ def on_button_packed():
     global line_iterator
     draw_lines()
 
+
 def change_to_distance(number):
-    x=(int(number[2])-1)*14.25 + int(number[0])%2 * 30 + 0.75
-    y=int(int(number[0])/2) * 32.2 + (int(number[1])-1)*4.6 + 0.65
+    x = (int(number[2])-1)*14.25 + int(number[0]) % 2 * 30 + 0.75
+    y = int(int(number[0])/2) * 32.2 + (int(number[1])-1)*4.6 + 0.65
     if number == "500" or number == "100":
         x = 53.25
-        if number =="500":
+        if number == "500":
             y = 86.85
         else:
             y = 24.65
-    return(x*11+60,y*11 + 85)
+    return (x*11+60, y*11 + 85)
+
 
 line_iterator = 0
 palet_iterator = 0
 
 list_of_navigation = route_tour
-list_of_future_placing = [[(0+line_iterator, 0+line_iterator), (200+line_iterator, 100+line_iterator)],
-                        [(200+line_iterator, 100+line_iterator), (300+line_iterator, 400+line_iterator)],
-                        [(150+line_iterator, 250+line_iterator), (10+line_iterator, 20+line_iterator)]]
+list_of_future_placing = [['100', '143', '133', '123', '02'], ['02', '123', '122', '132', '09'],
+                          ['09', '132', '142', '152', '162', '172',
+                           '312', '322', '321', '223', '222', '14'],
+                          ['14', '222', '212', '213', '223',
+                           '321', '331', '332', '342', '10'],
+                          ['10', '342', '352', '362', '372', '512', '522', '532', '542', '500']]
 list_of_past_placing = []
 
 
@@ -102,7 +152,6 @@ button_reached = Button(root, text="I am at package",
                         command=on_button_reached)
 button_packed = Button(root, text="Package is packed",
                        command=on_button_packed)
-button_undo = Button(root, text="Undo")
 
 plan_file = Image.open("plan.png")
 plan_width, plan_height = plan_file.size
@@ -121,11 +170,17 @@ worker_file = Image.open("worker.png")
 worker = ImageTk.PhotoImage(worker_file)
 worker_label = Label(image=worker, text="Worker")
 
+text_var = StringVar()
+text_var.set("Go to: H" + list_of_navigation[line_iterator][-2][0] + "0" + list_of_navigation[line_iterator][-2][1] + "A" + list_of_navigation[line_iterator][-1])
+
+# Create a Label widget and associate it with the StringVar
+text_label = Label(root, textvariable=text_var, font=("Helvetica", 16))
+
 
 warehouse_label.grid(column=0, rowspan=20)
 palet_label.grid(row=0, column=1)
 button_reached.grid(row=1, column=1)
 button_packed.grid(row=2, column=1)
-button_undo.grid(row=3, column=1)
+text_label.grid(row=3,column=1)
 
 root.mainloop()
